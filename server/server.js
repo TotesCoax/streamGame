ac466fc27262c60c1270a193add6af63cb980873
Chris K <cgkocur@gmail.com>
2021-08-31 18:27:02 -0400

    changes for deployment

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const io = require('socket.io')(httpServer, {
     serveClient: false,
     cors: {
       //I don't like using *, but figuring out what the hell was going wrong was too much of a bother when I just want to code a prototype.
-      origin: "https://calm-plateau-34573.herokuapp.com/",
+      origin: "*",
     }
   });
 

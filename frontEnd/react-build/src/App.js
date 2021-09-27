268280b45081fbdfa7650e398c1cc89a1a19d3f0
Chris K <cgkocur@gmail.com>
2021-09-27 17:30:29 -0400

    fyre helped me fix the react connection widePeepoHappy

--- frontEnd/react-build/src/App.js
+++ frontEnd/react-build/src/App.js
@@ -3,11 +3,19 @@ import io from 'socket.io-client'
 
 import Gameboard from "./components/Gameboard"
 
-const socket = io("https://calm-plateau-34573.herokuapp.com/", {transports: ['websocket', 'polling', 'flashsocket']})
+//const socket = io("https://calm-plateau-34573.herokuapp.com/", {transports: ['websocket', 'polling', 'flashsocket']})
 
 function App() {
+  const [socket, setSocket] = useState(null)
 
-  socket.on('init', (msg) => console.log(msg))
+  useEffect(() => {
+    const newSocket = io("https://calm-plateau-34573.herokuapp.com/", {transports: ['websocket', 'polling', 'flashsocket']})
+    newSocket.on('init', (msg) => console.log(msg))
+    setSocket(newSocket)
+    return () => newSocket.close()
+  }, [setSocket])
+
+  console.log(socket)
 
   return (
     <Gameboard />

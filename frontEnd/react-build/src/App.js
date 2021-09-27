4b1d9682b296352809d170391bf6ff7a320b30b8
Chris K <cgkocur@gmail.com>
2021-09-27 16:58:19 -0400

    more fiddling with react, troubleshooting

--- frontEnd/react-build/src/App.js
+++ frontEnd/react-build/src/App.js
@@ -3,14 +3,9 @@ import io from 'socket.io-client'
 
 import Gameboard from "./components/Gameboard"
 
-function App() {
-  const [socket, setSocket] = useState(null)
+const socket = io("https://calm-plateau-34573.herokuapp.com/", {transports: ['websocket', 'polling', 'flashsocket']})
 
-  useEffect(() => {
-    const newSocket = io("https://calm-plateau-34573.herokuapp.com/", {transports: ['websocket', 'polling', 'flashsocket']})
-    setSocket(newSocket)
-    return () => newSocket.close()
-  }, [setSocket])
+function App() {
 
   socket.on('init', (msg) => console.log(msg))
 

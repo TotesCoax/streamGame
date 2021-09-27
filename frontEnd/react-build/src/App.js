c6c433e61dc493beaa29a3f6564f1835bafb6731
Chris K <cgkocur@gmail.com>
2021-09-27 13:35:36 -0400

    initial react-backend connection testing

--- frontEnd/react-build/src/App.js
+++ frontEnd/react-build/src/App.js
@@ -1,6 +1,19 @@
+import { useEffect, useState } from "react";
+import io from 'socket.io-client'
+
 import Gameboard from "./components/Gameboard"
 
 function App() {
+  const [socket, setSocket] = useState(null)
+
+  useEffect(() => {
+    const newSocket = io("https://calm-plateau-34573.herokuapp.com/", {transports: ['websocket', 'polling', 'flashsocket']})
+    setSocket(newSocket)
+    return () => newSocket.close()
+  }, [setSocket])
+
+  socket.on('init', (msg) => console.log(msg))
+
   return (
     <Gameboard />
   )
